0ca324ecc91a3119688cf1ed54fe70a026f994a3
Daniela Muñoz <daniimhdez08@gmail.com>
2020-06-23 09:10:07 -0500

    Crear componente generico de listado de elementos

--- src/partials/_ListDefault.vue
+++ src/partials/_ListDefault.vue
@@ -1,8 +1,6 @@
 <template>
-    <div class="container">
-
-        
-        <div v-for="e in elements" v-bind:key="e.id">
+    <div>      
+        <div v-for="e in elementsList" v-bind:key="e.id">
             <router-link :to="'/detail/' + e.id">
             <b-card
                 :title="e.title"
@@ -13,16 +11,15 @@
             </b-card>
             </router-link>            
         </div>
-
     </div>
 </template>
 
 <script>
 export default {
 
-    created(){
-        this.findAll()
-    },
+    props:[
+        'elementsList'
+    ],
 
     data(){
         return{
@@ -31,12 +28,7 @@ export default {
         };
     },
     methods: {
-        findAll: function(){
-            fetch('http://127.0.0.1:8000/api/Element/?format=json')
-                .then(res => res.json())
-                //.then(res => console.log(res[0].id))
-                .then(res => this.elements =res)
-        }
+
     },
 }
 </script>
